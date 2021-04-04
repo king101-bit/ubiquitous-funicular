795c96b07d2e3d51bb22b1db5363d29ab0ca3581
Zack Unimashi <69579338+king101-bit@users.noreply.github.com>
2021-04-04 23:33:16 +0100

    Add files via upload

---
+++ Flur/main.js
@@ -0,0 +1,7 @@
+var currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
---
+++ Flur/main.ts
@@ -0,0 +1,9 @@
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
